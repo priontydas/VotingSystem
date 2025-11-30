774e4d13679c60d490d1976ffa766169b8f313e1
priontydas <priontydas84@gmail.com>
2025-11-30 10:59:30 +0600

    Create Main.java

---
+++ first update/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("ONLINE VOTING SYSTEM"); 
+        frame.setSize(800, 500);             
+        frame.setLayout(null);
+        frame.getContentPane().setBackground(new Color(240, 248, 255)); 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);   
+
+        // JLabel  title 
+        JLabel label = new JLabel("ONLINE VOTING SYSTEM", SwingConstants.CENTER);
+        label.setFont(new Font("Arial", Font.BOLD, 30));
+        label.setBounds(0, 50, 800, 50); 
+        frame.add(label);
+
+        frame.setVisible(true);              
+    }
+} 
